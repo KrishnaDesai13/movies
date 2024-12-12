58f3ba1643ce63b23d5fb94feb860cf8bc19c797
Krishna Desai <krishnadesai1311@gmail.com>
2024-12-12 16:55:08 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import HomePage from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <>
+      <p className="pagepara"><h3>WELCOME TO UPCOMING WEBSITE!!!</h3></p>
+    </>
+  );
+};
+
+export default HomePage;
